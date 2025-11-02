b09873fba0829809b8e4aca21e9aabdcc518898c
LiJie Su <opwinopwin@gmail.com>
2025-11-01 23:27:51 -0500

    add contents of indexpage

--- report-generator-app/src/App.js
+++ report-generator-app/src/App.js
@@ -1,9 +1,14 @@
 import CoverPage from './CommonPage/CoverPage';
+import ContentsIndexPage from './CommonPage/ContentsIndexPage';
+import './StyleScript/Restricted_Report_Style.css';
 
 function App() {
   return (
     <div className="App">
       <CoverPage />
+      <div className="restricted-report-content">
+        <ContentsIndexPage />
+      </div>
     </div>
   );
 }
---
+++ report-generator-app/src/CommonPage/ContentsIndexPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ContentsIndexPage = () => {
+  return (
+    <div className="contents-index-page">
+      <div className="page-content">
+        {/* Contents Index Page specific content */}
+        <h2>Table of Contents</h2>
+        <ul>
+          <li>Section 1</li>
+          <li>Section 2</li>
+          <li>Section 3</li>
+        </ul>
+      </div>
+
+    </div>
+  );
+};
+
+export default ContentsIndexPage;

30e893206a3dc61f3f8609e59bba040ab312d7b5
LiJie Su <opwinopwin@gmail.com>
2025-11-02 20:35:57 -0600

    add kpi page and about page

--- report-generator-app/src/App.js
+++ report-generator-app/src/App.js
@@ -5,6 +5,8 @@ import DeviceInfoPage from './CommonPage/DeviceInfoPage';
 import ReportHeader from './CommonPage/ReportHeader';
 import ReportFooter from './CommonPage/ReportFooter';
 import CallPerformanceDetails from './ReportDetails/CallPerformance/CallPerformanceDetails';
+import KpiPage from './CommonPage/KpiPage';
+import AboutPage from './CommonPage/AboutPage';
 
 import './StyleScript/Restricted_Report_Style.css';
 
@@ -18,6 +20,8 @@ function App() {
       <ContentsIndexPage />
       <SummaryPage />
       <CallPerformanceDetails />
+      <KpiPage />
+      <AboutPage />
       <ReportFooter />
     </div>
   );
---
+++ report-generator-app/src/CommonPage/KpiPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KpiPage = () => {
+  return (
+    <div className='page-content'>
+      <h1>KPI Page</h1>
+    </div>
+  );
+};
+
+export default KpiPage;

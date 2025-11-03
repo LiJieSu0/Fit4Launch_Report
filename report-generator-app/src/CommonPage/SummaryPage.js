8069fc133cb1083aad22d0bd7e2fcd3a3a4d8662
LiJie Su <opwinopwin@gmail.com>
2025-11-02 20:11:45 -0600

    modify title margin

--- report-generator-app/src/CommonPage/SummaryPage.js
+++ report-generator-app/src/CommonPage/SummaryPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SummaryPage = () => {
   return (
     <div className="page-content">
-      <h1>Summary Page</h1>
+      <h2>Summary Page</h2>
       <p>This is the placeholder for the Summary Page content.</p>
     </div>
   );

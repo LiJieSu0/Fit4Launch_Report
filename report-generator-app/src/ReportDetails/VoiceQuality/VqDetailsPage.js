1f418944441287ece86fa9d5df3f69e69d93790b
LiJie Su <opwinopwin@gmail.com>
2025-11-03 16:07:25 -0600

    add vq details structure

--- report-generator-app/src/ReportDetails/VoiceQuality/VqDetailsPage.js
+++ report-generator-app/src/ReportDetails/VoiceQuality/VqDetailsPage.js
@@ -8,15 +8,19 @@ const VqDetailsPage = () => {
   return (
     <div>
         <div className="page-content">
+            <h2>3.1 5G Auto VoNR Enabled AMR NB VQ</h2>
             {/* 3.1 table area */}
         </div>
         <div className="page-content">
+            <h2>3.2 5G Auto VoNR Enabled AMR WB VQ</h2>
             {/* 3.2 table area */}
         </div>
         <div className="page-content">
+            <h2>3.3 5G Auto VoNR Disabled EVS WB VQ</h2>
             {/* 3.3 table area */}
         </div>
         <div className="page-content">
+            <h2>3.4 5G Auto VoNR Enabled EVS WB VQ</h2>
             {/* 3.4 table area */}
         </div>
     </div>

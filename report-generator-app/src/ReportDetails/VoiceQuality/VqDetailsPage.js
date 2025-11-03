6288014274e5f978e996f966e6e0413f617913ae
LiJie Su <opwinopwin@gmail.com>
2025-11-03 16:06:01 -0600

    vq details page

---
+++ report-generator-app/src/ReportDetails/VoiceQuality/VqDetailsPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../StyleScript/Restricted_Report_Style.css';
+import voiceQualityResults from '../../DataFiles/VoiceQualityResults.json';
+
+const VqDetailsPage = () => {
+  
+
+  return (
+    <div>
+        <div className="page-content">
+            {/* 3.1 table area */}
+        </div>
+        <div className="page-content">
+            {/* 3.2 table area */}
+        </div>
+        <div className="page-content">
+            {/* 3.3 table area */}
+        </div>
+        <div className="page-content">
+            {/* 3.4 table area */}
+        </div>
+    </div>
+    
+  );
+};
+
+export default VqDetailsPage;

d77f2c9905975ab72e2a3b65c7f9db9cb4543082
LiJie Su <opwinopwin@gmail.com>
2025-11-07 11:24:55 -0600

    add ms data

--- report-generator-app/src/ReportDetails/DataPerformance/DpDetailsPage.js
+++ report-generator-app/src/ReportDetails/DataPerformance/DpDetailsPage.js
@@ -3,6 +3,7 @@ import DpRangeChart from './DpRangeChart';
 import '../../StyleScript/Restricted_Report_Style.css';
 import DpDetailsTableLoc3 from './DpDetailsTableLoc3';
 import httpSS_Stationary_DL_Data from '../../DataFiles/SA/DpStationaryResults/Single Stream HTTP.json';
+import httpMS_Stationary_Data from '../../DataFiles/SA/DpStationaryResults/Multi Stream HTTP.json';
 
 function DpDetailsPage() {
   const httpSS_Stationary_DL = {
@@ -35,9 +36,36 @@ function DpDetailsPage() {
     },
   };
 
-  const httpMS_Stationary_DL ={}
 
-  const httpMS_Stationary_UL={}
+  const httpMS_Stationary_DL = {
+    Good: {
+      DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_MS HTTP DL_Good"].Throughput,
+      REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_MS HTTP DL_Good"].Throughput,
+    },
+    Moderate: {
+      DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
+      REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
+    },
+    Poor: {
+      DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
+      REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
+    },
+  };
+
+  const httpMS_Stationary_UL = {
+    Good: {
+      DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+      REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+    },
+    Moderate: {
+      DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_DUT_"].Throughput,
+      REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_REF_"].Throughput,
+    },
+    Poor: {
+      DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["dut_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
+      REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["ref_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
+    },
+  };
 
   return (
     <div>
@@ -54,8 +82,6 @@ function DpDetailsPage() {
       <h2>HTTP Multi Stream test - 5G NR</h2>
       <DpDetailsTableLoc3 data={httpMS_Stationary_DL} tableName="Multi Stream HTTP Download for 30 seconds"  />
       <DpDetailsTableLoc3 data={httpMS_Stationary_UL} tableName="Multi Stream HTTP Upload for 30 seconds"  />
-
-
       </div>
       <div className='page-content'>
       <h2>UDP test - 5G NR</h2>

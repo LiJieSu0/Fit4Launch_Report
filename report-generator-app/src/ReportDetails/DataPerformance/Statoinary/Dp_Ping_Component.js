7b240e37de4ed0e8c9e994bd4fa553d93c4e5488
LiJie Su <opwinopwin@gmail.com>
2025-11-07 15:54:03 -0600

    add ping data

--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_Ping_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_Ping_Component.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import DpPingTableLoc3 from './Table/DpPingTableLoc3';
+import PingData from '../../../DataFiles/SA/DpStationaryResults/Ping.json';
 
 function Dp_Ping_Component() {
+    const Dp_Ping_Data = PingData;
+
+    const processedPingData = {
+        average: { DUT: {}, REF: {} },
+        std_dev: { DUT: {}, REF: {} },
+        max: { DUT: {}, REF: {} },
+        min: { DUT: {}, REF: {} }
+    };
+
+    const locations = ["Good", "Moderate", "Poor"];
+    const deviceTypes = ["DUT", "REF"];
+
+    locations.forEach(location => {
+        const pingCategory = Dp_Ping_Data[location]["25x64 bytes PING"];
+        for (const key in pingCategory) {
+            if (pingCategory.hasOwnProperty(key)) {
+                const deviceData = pingCategory[key];
+                const deviceType = deviceData["Device Type"];
+                const pingRTT = deviceData["Ping RTT"];
+
+                if (deviceType === "DUT") {
+                    processedPingData.average.DUT[location] = pingRTT.avg.toFixed(2);
+                    processedPingData.std_dev.DUT[location] = pingRTT.std_dev.toFixed(2);
+                    processedPingData.max.DUT[location] = pingRTT.max.toFixed(2);
+                    processedPingData.min.DUT[location] = pingRTT.min.toFixed(2);
+                } else if (deviceType === "REF") {
+                    processedPingData.average.REF[location] = pingRTT.avg.toFixed(2);
+                    processedPingData.std_dev.REF[location] = pingRTT.std_dev.toFixed(2);
+                    processedPingData.max.REF[location] = pingRTT.max.toFixed(2);
+                    processedPingData.min.REF[location] = pingRTT.min.toFixed(2);
+                }
+            }
+        }
+    });
+
+    deviceTypes.forEach(deviceType => {
+        let avgSum = 0;
+        let stdDevSum = 0;
+        let maxSum = 0;
+        let minSum = 0;
+        let count = 0;
+
+        locations.forEach(location => {
+            if (processedPingData.average[deviceType][location] !== undefined) {
+                avgSum += parseFloat(processedPingData.average[deviceType][location]);
+                stdDevSum += parseFloat(processedPingData.std_dev[deviceType][location]);
+                maxSum += parseFloat(processedPingData.max[deviceType][location]);
+                minSum += parseFloat(processedPingData.min[deviceType][location]);
+                count++;
+            }
+        });
+
+        if (count > 0) {
+            processedPingData.average[deviceType].Overall = (avgSum / count).toFixed(2);
+            processedPingData.std_dev[deviceType].Overall = (stdDevSum / count).toFixed(2);
+            processedPingData.max[deviceType].Overall = (maxSum / count).toFixed(2);
+            processedPingData.min[deviceType].Overall = (minSum / count).toFixed(2);
+        }
+    });
+
     return(
         <div>
             <div className='page-content'>
             <h2>PING test - 5G NR</h2>
-            <DpPingTableLoc3 />
+            <DpPingTableLoc3 data={processedPingData} />
             </div>
         </div>
     )
--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPingTableLoc3.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPingTableLoc3.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import '../../../../StyleScript/Restricted_Report_Style.css';
 
-const DpPingTableLoc3 = () => {
+const DpPingTableLoc3 = ({ data }) => {
+    if (!data || Object.keys(data).length === 0) {
+        return (
+            <div className="">
+                <table className="device-info-table dp-details-table">
+                    <thead>
+                        <tr>
+                            <th rowSpan="2">Ping RTT (ms)</th>
+                            <th rowSpan="2">Device Name</th>
+                            <th rowSpan="2">Overall</th>
+                            <th colSpan="3">Location</th>
+                        </tr>
+                        <tr>
+                            <th>Good</th>
+                            <th>Moderate</th>
+                            <th>Poor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td colSpan="6">No data available</td></tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
     return (
         <div className="">
             <table className="device-info-table dp-details-table">
@@ -22,62 +47,62 @@ const DpPingTableLoc3 = () => {
                     <tr>
                         <td rowSpan="2">Average</td>
                         <td>DUT</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.average.DUT.Overall}</td>
+                        <td>{data.average.DUT.Good}</td>
+                        <td>{data.average.DUT.Moderate}</td>
+                        <td>{data.average.DUT.Poor}</td>
                     </tr>
                     <tr>
                         <td>REF</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.average.REF.Overall}</td>
+                        <td>{data.average.REF.Good}</td>
+                        <td>{data.average.REF.Moderate}</td>
+                        <td>{data.average.REF.Poor}</td>
                     </tr>
                     <tr>
                         <td rowSpan="2">Standard Deviation</td>
                         <td>DUT</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.std_dev.DUT.Overall}</td>
+                        <td>{data.std_dev.DUT.Good}</td>
+                        <td>{data.std_dev.DUT.Moderate}</td>
+                        <td>{data.std_dev.DUT.Poor}</td>
                     </tr>
                     <tr>
                         <td>REF</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.std_dev.REF.Overall}</td>
+                        <td>{data.std_dev.REF.Good}</td>
+                        <td>{data.std_dev.REF.Moderate}</td>
+                        <td>{data.std_dev.REF.Poor}</td>
                     </tr>
                     <tr>
                         <td rowSpan="2">Maximum</td>
                         <td>DUT</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.max.DUT.Overall}</td>
+                        <td>{data.max.DUT.Good}</td>
+                        <td>{data.max.DUT.Moderate}</td>
+                        <td>{data.max.DUT.Poor}</td>
                     </tr>
                     <tr>
                         <td>REF</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.max.REF.Overall}</td>
+                        <td>{data.max.REF.Good}</td>
+                        <td>{data.max.REF.Moderate}</td>
+                        <td>{data.max.REF.Poor}</td>
                     </tr>
                     <tr>
                         <td rowSpan="2">Minimum</td>
                         <td>DUT</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.min.DUT.Overall}</td>
+                        <td>{data.min.DUT.Good}</td>
+                        <td>{data.min.DUT.Moderate}</td>
+                        <td>{data.min.DUT.Poor}</td>
                     </tr>
                     <tr>
                         <td>REF</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{data.min.REF.Overall}</td>
+                        <td>{data.min.REF.Good}</td>
+                        <td>{data.min.REF.Moderate}</td>
+                        <td>{data.min.REF.Poor}</td>
                     </tr>
                 </tbody>
             </table>

bfff25e6817859f733deb1fe387960bac8ca6910
LiJie Su <opwinopwin@gmail.com>
2025-11-10 19:28:41 -0600

    add playstore table component

--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
@@ -1,52 +1,10 @@
 import React from 'react';
-import '../../../../src/StyleScript/Restricted_Report_Style.css';
+import DpPlayStoreTable from './Table/DpPlayStoreTable';
 
 const Dp_playStore_Component = () => {
-  const tableData = [
-    { market: 'Seattle', throughput: '30MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '30MB', deviceName: 'REF' },
-    { market: 'Seattle', throughput: '60MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '60MB', deviceName: 'REF' },
-    { market: 'Seattle', throughput: '100MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '100MB', deviceName: 'REF' },
-  ];
-
   return (
     <div className="">
-      <table className="general-table-style">
-        <thead>
-          <tr>
-            <th rowSpan="2">Market</th>
-            <th rowSpan="2">Throughput (kbps)</th>
-            <th rowSpan="2">Device Name</th>
-          </tr>
-          <tr>
-            <th>Overall</th>
-            <th>Stationary - Site 1</th>
-            <th>Stationary - Site 2</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row, index) => (
-            <tr key={index}>
-              {index % 2 === 0 && (
-                <td rowSpan={2} style={{ border: '1px solid black', backgroundColor: 'white' }}>
-                  {row.market}
-                </td>
-              )}
-              {index % 2 === 0 && (
-                <td rowSpan={2} style={{ border: '1px solid black', backgroundColor: 'white' }}>
-                  {row.throughput}
-                </td>
-              )}
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.deviceName}</td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DpPlayStoreTable />
     </div>
   );
 };

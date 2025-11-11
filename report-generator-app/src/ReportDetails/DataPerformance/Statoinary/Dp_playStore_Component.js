b3ffbe6b2eb00307d72fee05f7cb0a6579c64d0c
LiJie Su <opwinopwin@gmail.com>
2025-11-10 19:44:34 -0600

    add playstore data

--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
@@ -3,9 +3,10 @@ import DpPlayStoreTable from './Table/DpPlayStoreTable';
 
 const Dp_playStore_Component = () => {
   return (
-    <div className="">
-      <DpPlayStoreTable />
-    </div>
+      <div className='page-content'>
+        <h2>Play-store app download test - 5G NR</h2>
+        <DpPlayStoreTable />
+      </div>
   );
 };
 
--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPlayStoreTable.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPlayStoreTable.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import '../../../../../src/StyleScript/Restricted_Report_Style.css';
+import playStoreData from '../../../../DataFiles/SA/DpPlayStoreResults/Play Store.json';
 
 const DpPlayStoreTable = () => {
-  const tableData = [
-    { market: 'Seattle', throughput: '30MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '30MB', deviceName: 'REF' },
-    { market: 'Seattle', throughput: '60MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '60MB', deviceName: 'REF' },
-    { market: 'Seattle', throughput: '100MB', deviceName: 'DUT' },
-    { market: 'Seattle', throughput: '100MB', deviceName: 'REF' },
-  ];
+  const processData = () => {
+    const processed = [];
+    const throughputCategories = ['30M', '60M', '100M'];
+    const deviceTypes = ['DUT', 'REF'];
+    const locations = ['location1', 'location2', 'location3'];
+
+    throughputCategories.forEach(throughput => {
+      deviceTypes.forEach(device => {
+        const rowData = {
+          market: 'Seattle', // Assuming market is always Seattle for now
+          throughput: throughput,
+          deviceName: device,
+          site1: null,
+          site2: null,
+          site3: null,
+          overall: null,
+        };
+
+        let siteThroughputs = [];
+
+        locations.forEach((location, index) => {
+          const siteKey = `site${index + 1}`;
+          if (playStoreData[location] && playStoreData[location][device] && playStoreData[location][device][throughput]) {
+            const dataKey = Object.keys(playStoreData[location][device][throughput])[0];
+            const value = playStoreData[location][device][throughput][dataKey].overall_average_throughput;
+            rowData[siteKey] = value !== undefined ? value.toFixed(2) : 'N/A';
+            if (value !== undefined) {
+              siteThroughputs.push(value);
+            }
+          } else {
+            rowData[siteKey] = 'N/A';
+          }
+        });
+
+        if (siteThroughputs.length > 0) {
+          const sum = siteThroughputs.reduce((acc, val) => acc + val, 0);
+          rowData.overall = (sum / siteThroughputs.length).toFixed(2);
+        } else {
+          rowData.overall = 'N/A';
+        }
+        processed.push(rowData);
+      });
+    });
+    return processed;
+  };
+
+  const tableData = processData();
 
   return (
     <div className="">
@@ -41,10 +81,10 @@ const DpPlayStoreTable = () => {
                 </td>
               )}
               <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.deviceName}</td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
-              <td style={{ border: '1px solid black', backgroundColor: 'white' }}></td>
+              <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.overall}</td>
+              <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.site1}</td>
+              <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.site2}</td>
+              <td style={{ border: '1px solid black', backgroundColor: 'white' }}>{row.site3}</td>
             </tr>
           ))}
         </tbody>

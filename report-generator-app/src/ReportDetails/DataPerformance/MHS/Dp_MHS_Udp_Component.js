1abe0bbad5c56cbf47497835c81431b0303290d7
LiJie Su <opwinopwin@gmail.com>
2025-11-10 20:32:29 -0600

    remove udp table from MHS

--- report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_Udp_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_Udp_Component.js
@@ -4,18 +4,12 @@ import DpMHSUdpTable from "./Table/DpMHSUdpTable";
 function Dp_MHS_Udp_Component() {
   // Placeholder for data fetching or state management
   const data = [
-    { metric: "Throughput", idealThroughput: "High", deviceName: "DUT", location: { good: 0, moderate: 0 } },
-    { metric: "Throughput", idealThroughput: "High", deviceName: "REF", location: { good: 0, moderate: 0 } },
-    { metric: "Throughput", idealThroughput: "Medium", deviceName: "DUT", location: { good: 0, moderate: 0 } },
-    { metric: "Throughput", idealThroughput: "Medium", deviceName: "REF", location: { good: 0, moderate: 0 } },
-    { metric: "Throughput", idealThroughput: "Low", deviceName: "DUT", location: { good: 0, moderate: 0 } },
-    { metric: "Throughput", idealThroughput: "Low", deviceName: "REF", location: { good: 0, moderate: 0 } },
+
   ];
 
   return (
     <div className='page-content'>
       <h2>MHS-UDP Component</h2>
-      <DpMHSUdpTable data={data} tableName="MHS UDP Throughput" />
     </div>
   );
 }

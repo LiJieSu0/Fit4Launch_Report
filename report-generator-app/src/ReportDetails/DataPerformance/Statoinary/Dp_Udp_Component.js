339f15a87ce5b70290814e2301f2e593fb11d48c
LiJie Su <opwinopwin@gmail.com>
2025-11-12 12:32:47 -0600

    add udp histogram

--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_Udp_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_Udp_Component.js
@@ -1,9 +1,88 @@
 import React from 'react';
 import DpUdpTableLoc3 from './Table/DpUdpTableLoc3';
+import DpHistogramComponent from '../DpHistogramComponent';
 import udp_Stationary_Data from '../../../DataFiles/SA/DpStationaryResults/UDP.json';
+import { CHART_COLOR_DUT, CHART_COLOR_REF } from '../../../Constants/ChartColors';
 import '../../../StyleScript/Restricted_Report_Style.css';
 
 function Dp_Udp_Component() {
+  // Extract DL Mean Throughput data
+  const dlMeanThroughput200_DUT = udp_Stationary_Data.Good["UDP Download Task at 200 Mbps for 10 seconds"]["dut_5G auto_UDP DL 200M 10sec_Good"].Throughput.Mean;
+  const dlMeanThroughput200_REF = udp_Stationary_Data.Good["UDP Download Task at 200 Mbps for 10 seconds"]["ref_5G auto_UDP DL 200M 10sec_Good"].Throughput.Mean;
+  const dlMeanThroughput400_DUT = udp_Stationary_Data.Good["UDP Download Task at 400 Mbps for 10 seconds"]["dut_5G auto_UDP DL 400M 10sec_Good"].Throughput.Mean;
+  const dlMeanThroughput400_REF = udp_Stationary_Data.Good["UDP Download Task at 400 Mbps for 10 seconds"]["ref_5G auto_UDP DL 400M 10sec_Good"].Throughput.Mean;
+
+  const dlMeanThroughput200_DUT_Moderate = udp_Stationary_Data.Moderate["UDP Download Task at 200 Mbps for 10 seconds"]["dut_5G auto_UDP Download Task at 200 Mbps for 10"].Throughput.Mean;
+  const dlMeanThroughput200_REF_Moderate = udp_Stationary_Data.Moderate["UDP Download Task at 200 Mbps for 10 seconds"]["ref_5G auto_UDP Download Task at 200 Mbps for 10"].Throughput.Mean;
+  const dlMeanThroughput400_DUT_Moderate = udp_Stationary_Data.Moderate["UDP Download Task at 400 Mbps for 10 seconds"]["dut_5G auto_UDP Download Task at 400 Mbps for 10 seconds"].Throughput.Mean;
+  const dlMeanThroughput400_REF_Moderate = udp_Stationary_Data.Moderate["UDP Download Task at 400 Mbps for 10 seconds"]["ref_5G auto_UDP Download Task at 400 Mbps for 10 seconds"].Throughput.Mean;
+
+  const dlMeanThroughput200_DUT_Poor = udp_Stationary_Data.Poor["UDP Download Task at 200 Mbps for 10 seconds"]["dut_5G auto_UDP Download Task at 200 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+  const dlMeanThroughput200_REF_Poor = udp_Stationary_Data.Poor["UDP Download Task at 200 Mbps for 10 seconds"]["ref_5G auto_UDP Download Task at 200 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+  const dlMeanThroughput400_DUT_Poor = udp_Stationary_Data.Poor["UDP Download Task at 400 Mbps for 10 seconds"]["dut_5G auto_UDP Download Task at 400 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+  const dlMeanThroughput400_REF_Poor = udp_Stationary_Data.Poor["UDP Download Task at 400 Mbps for 10 seconds"]["ref_5G auto_UDP Download Task at 400 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+
+
+  // Calculate average for each location (Good, Moderate, Poor) for DL
+  const dlGoodDUT = (dlMeanThroughput200_DUT + dlMeanThroughput400_DUT) / 2;
+  const dlModerateDUT = (dlMeanThroughput200_DUT_Moderate + dlMeanThroughput400_DUT_Moderate) / 2;
+  const dlPoorDUT = (dlMeanThroughput200_DUT_Poor + dlMeanThroughput400_DUT_Poor) / 2;
+
+  const dlGoodREF = (dlMeanThroughput200_REF + dlMeanThroughput400_REF) / 2;
+  const dlModerateREF = (dlMeanThroughput200_REF_Moderate + dlMeanThroughput400_REF_Moderate) / 2;
+  const dlPoorREF = (dlMeanThroughput200_REF_Poor + dlMeanThroughput400_REF_Poor) / 2;
+
+  const dlHistogramData = [
+    { name: 'Good', DUT: dlGoodDUT, REF: dlGoodREF },
+    { name: 'Moderate', DUT: dlModerateDUT, REF: dlModerateREF },
+    { name: 'Poor', DUT: dlPoorDUT, REF: dlPoorREF },
+    { name: 'Overall',
+      DUT: (dlGoodDUT + dlModerateDUT + dlPoorDUT) / 3,
+      REF: (dlGoodREF + dlModerateREF + dlPoorREF) / 3
+    },
+  ];
+
+  // Extract UL Throughput data
+  const ulThroughput10_DUT = udp_Stationary_Data.Good["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH01_TMO-dut_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_Good Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput10_REF = udp_Stationary_Data.Good["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH02_TMO-ref_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_Good Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput20_DUT = udp_Stationary_Data.Good["UDP Upload Task at 20 Mbps for 10 seconds"]["_CH01_TMO-dut_5G auto_UDP Upload Task at 20 Mbps for 10 seconds_Good Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput20_REF = udp_Stationary_Data.Good["UDP Upload Task at 20 Mbps for 10 seconds"]["_CH02_TMO-ref_5G auto_UDP Upload Task at 20 Mbps for 10 seconds_Good Coverage_DA Test"].Throughput.Mean;
+
+  const ulThroughput10_DUT_Moderate = udp_Stationary_Data.Moderate["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH01_TMO_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_DUT_"].Throughput.Mean;
+  const ulThroughput10_REF_Moderate = udp_Stationary_Data.Moderate["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH02_TMO_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_REF_"].Throughput.Mean;
+  const ulThroughput20_DUT_Moderate = udp_Stationary_Data.Moderate["UDP Upload Task at 20 Mbps for 10 seconds"]["DUT_5G auto_UDP Upload Task at 20 Mbps for 10 sec"].Throughput.Mean;
+  const ulThroughput20_REF_Moderate = udp_Stationary_Data.Moderate["UDP Upload Task at 20 Mbps for 10 seconds"]["REF_5G auto_UDP Upload Task at 20 Mbps for 10 sec"].Throughput.Mean;
+
+  const ulThroughput10_DUT_Poor = udp_Stationary_Data.Poor["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH01_TMO-dut_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput10_REF_Poor = udp_Stationary_Data.Poor["UDP Upload Task at 10 Mbps for 10 seconds"]["_CH02_TMO-ref_5G auto_UDP Upload Task at 10 Mbps for 10 seconds_Poor Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput20_DUT_Poor = udp_Stationary_Data.Poor["UDP Upload Task at 20 Mbps for 10 seconds"]["DUT UDP Upload Task at 20 Mbps for 10 seconds_poor Coverage_DA Test"].Throughput.Mean;
+  const ulThroughput20_REF_Poor = udp_Stationary_Data.Poor["UDP Upload Task at 20 Mbps for 10 seconds"]["REF UDP Upload Task at 20 Mbps for 10 seconds_poor Coverage_DA Test"].Throughput.Mean;
+
+
+  // Calculate average for each location (Good, Moderate, Poor) for UL
+  const ulGoodDUT = (ulThroughput10_DUT + ulThroughput20_DUT) / 2;
+  const ulModerateDUT = (ulThroughput10_DUT_Moderate + ulThroughput20_DUT_Moderate) / 2;
+  const ulPoorDUT = (ulThroughput10_DUT_Poor + ulThroughput20_DUT_Poor) / 2;
+
+  const ulGoodREF = (ulThroughput10_REF + ulThroughput20_REF) / 2;
+  const ulModerateREF = (ulThroughput10_REF_Moderate + ulThroughput20_REF_Moderate) / 2;
+  const ulPoorREF = (ulThroughput10_REF_Poor + ulThroughput20_REF_Poor) / 2;
+
+  const ulHistogramData = [
+    { name: 'Good', DUT: ulGoodDUT, REF: ulGoodREF },
+    { name: 'Moderate', DUT: ulModerateDUT, REF: ulModerateREF },
+    { name: 'Poor', DUT: ulPoorDUT, REF: ulPoorREF },
+    { name: 'Overall',
+      DUT: (ulGoodDUT + ulModerateDUT + ulPoorDUT) / 3,
+      REF: (ulGoodREF + ulModerateREF + ulPoorREF) / 3
+    },
+  ];
+
+  const barKeys = [
+    { key: 'DUT', fill: CHART_COLOR_DUT },
+    { key: 'REF', fill: CHART_COLOR_REF },
+  ];
+
   const udp_Stationary_DL = [
     // Mean Throughput - 200 Mbps
     
@@ -350,9 +429,21 @@ function Dp_Udp_Component() {
       <div className='page-content'>
         <h2>UDP test - 5G NR</h2>
         <DpUdpTableLoc3 data={udp_Stationary_DL} tableName="UDP DL" />
+        <DpHistogramComponent
+          data={dlHistogramData}
+          title="UDP Download Throughput"
+          yAxisLabel="Throughput (Mbps)"
+          barKeys={barKeys}
+        />
       </div>
       <div className='page-content'>
         <DpUdpTableLoc3 data={udp_Stationary_UL} tableName="UDP UL" />
+        <DpHistogramComponent
+          data={ulHistogramData}
+          title="UDP Upload Throughput"
+          yAxisLabel="Throughput (Mbps)"
+          barKeys={barKeys}
+        />
       </div>
     </>
   );
--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_httpMS_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_httpMS_Component.js
@@ -1,43 +1,85 @@
 import React from 'react';
 import '../../../StyleScript/Restricted_Report_Style.css';
 import DpDetailsTableLoc3 from './Table/DpDetailsTableLoc3';
+import DpHistogramComponent from '../DpHistogramComponent';
+import { CHART_COLOR_DUT, CHART_COLOR_REF } from '../../../Constants/ChartColors';
 import httpMS_Stationary_Data from '../../../DataFiles/SA/DpStationaryResults/Multi Stream HTTP.json';
 
-const Dp_httpMS_Component = () => {
+function Dp_httpMS_Component() {
     const httpMS_Stationary_DL = {
-    Good: {
-      DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_MS HTTP DL_Good"].Throughput,
-      REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_MS HTTP DL_Good"].Throughput,
-    },
-    Moderate: {
-      DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
-      REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
-    },
-    Poor: {
-      DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
-      REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
-    },
+        Good: {
+            DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_MS HTTP DL_Good"].Throughput,
+            REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_MS HTTP DL_Good"].Throughput,
+        },
+        Moderate: {
+            DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["dut_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
+            REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Download for 30 seconds"]["ref_5G auto_Multi Stream HTTP Download for 30 seconds"].Throughput,
+        },
+        Poor: {
+            DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
+            REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Download for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Download for 30 seconds_Poor Coverage_DA Test"].Throughput,
+        },
     };
 
     const httpMS_Stationary_UL = {
-    Good: {
-      DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
-      REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
-    },
-    Moderate: {
-      DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_DUT_"].Throughput,
-      REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_REF_"].Throughput,
-    },
-    Poor: {
-      DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["dut_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
-      REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["ref_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
-    },
+        Good: {
+            DUT: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO-dut_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+            REF: httpMS_Stationary_Data.Good["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO-ref_5G auto_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+        },
+        Moderate: {
+            DUT: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH01_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_DUT_"].Throughput,
+            REF: httpMS_Stationary_Data.Moderate["Multi Stream HTTP Upload for 30 seconds"]["_CH02_TMO_5G auto_Multi Stream HTTP Upload for 30 seconds_REF_"].Throughput,
+        },
+        Poor: {
+            DUT: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["dut_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
+            REF: httpMS_Stationary_Data.Poor["Multi Stream HTTP Upload for 30 seconds"]["ref_5g auto_Multi Stream HTTP Upload for 30 seconds_poor Coverage_DA Test"].Throughput,
+        },
     };
+
+    const dlHistogramData = [
+        { name: 'Good', DUT: httpMS_Stationary_DL.Good.DUT.Mean, REF: httpMS_Stationary_DL.Good.REF.Mean },
+        { name: 'Moderate', DUT: httpMS_Stationary_DL.Moderate.DUT.Mean, REF: httpMS_Stationary_DL.Moderate.REF.Mean },
+        { name: 'Poor', DUT: httpMS_Stationary_DL.Poor.DUT.Mean, REF: httpMS_Stationary_DL.Poor.REF.Mean },
+        {
+            name: 'Overall',
+            DUT: (httpMS_Stationary_DL.Good.DUT.Mean + httpMS_Stationary_DL.Moderate.DUT.Mean + httpMS_Stationary_DL.Poor.DUT.Mean) / 3,
+            REF: (httpMS_Stationary_DL.Good.REF.Mean + httpMS_Stationary_DL.Moderate.REF.Mean + httpMS_Stationary_DL.Poor.REF.Mean) / 3
+        },
+    ];
+
+    const ulHistogramData = [
+        { name: 'Good', DUT: httpMS_Stationary_UL.Good.DUT.Mean, REF: httpMS_Stationary_UL.Good.REF.Mean },
+        { name: 'Moderate', DUT: httpMS_Stationary_UL.Moderate.DUT.Mean, REF: httpMS_Stationary_UL.Moderate.REF.Mean },
+        { name: 'Poor', DUT: httpMS_Stationary_UL.Poor.DUT.Mean, REF: httpMS_Stationary_UL.Poor.REF.Mean },
+        {
+            name: 'Overall',
+            DUT: (httpMS_Stationary_UL.Good.DUT.Mean + httpMS_Stationary_UL.Moderate.DUT.Mean + httpMS_Stationary_UL.Poor.DUT.Mean) / 3,
+            REF: (httpMS_Stationary_UL.Good.REF.Mean + httpMS_Stationary_UL.Moderate.REF.Mean + httpMS_Stationary_UL.Poor.REF.Mean) / 3
+        },
+    ];
+
+    const barKeys = [
+        { key: 'DUT', fill: CHART_COLOR_DUT },
+        { key: 'REF', fill: CHART_COLOR_REF },
+    ];
+
     return (
         <div className='page-content'>
-        <h2>HTTP Multi Stream test - 5G NR</h2>
-        <DpDetailsTableLoc3 data={httpMS_Stationary_DL} tableName="Multi Stream HTTP Download for 30 seconds"  />
-        <DpDetailsTableLoc3 data={httpMS_Stationary_UL} tableName="Multi Stream HTTP Upload for 30 seconds"  />
+            <h2>HTTP Multi Stream test - 5G NR</h2>
+            <DpDetailsTableLoc3 data={httpMS_Stationary_DL} tableName="Multi Stream HTTP Download for 30 seconds" />
+            <DpHistogramComponent
+                data={dlHistogramData}
+                title="Multi Stream HTTP Download Throughput"
+                yAxisLabel="Throughput (Mbps)"
+                barKeys={barKeys}
+            />
+            <DpDetailsTableLoc3 data={httpMS_Stationary_UL} tableName="Multi Stream HTTP Upload for 30 seconds" />
+            <DpHistogramComponent
+                data={ulHistogramData}
+                title="Multi Stream HTTP Upload Throughput"
+                yAxisLabel="Throughput (Mbps)"
+                barKeys={barKeys}
+            />
         </div>
     );
 };

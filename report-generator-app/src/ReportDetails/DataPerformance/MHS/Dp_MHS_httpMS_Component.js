b62b1d991a6e0c57ea455159559189326d9ac9af
LiJie Su <opwinopwin@gmail.com>
2025-11-12 13:11:26 -0600

    add mhs ms ul

--- report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpMS_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpMS_Component.js
@@ -20,9 +20,26 @@ function Dp_MHS_httpMS_Component() {
   return (
     <div className='page-content'>
       <h2>MHS-httpMS Component</h2>
-      <DpMHSHttpMSTable data={data} tableName="MHS Multi Stream HTTP Throughput" />
+      <DpMHSHttpMSTable data={data} tableName="MHS Multi Stream HTTP Download Throughput" />
+      {/* dl histogram */}
+      <DpMHSHttpMSTable data={dataUL} tableName="MHS Multi Stream HTTP Upload Throughput" />
+      {/* ul histogram */}
     </div>
   );
 }
+
+const uploadGoodData = MultiStreamHTTPData.Good["Multi Stream HTTP Upload for 30 seconds"];
+const uploadModerateData = MultiStreamHTTPData.Moderate["Multi Stream HTTP Upload for 30 seconds"];
+
+const dataUL = {
+  Good: {
+    DUT: uploadGoodData["_20250915_120237_CH01_TMO-DUT_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+    REF: uploadGoodData["_20250915_120237_CH02_TMO-Ref_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput,
+  },
+  Moderate: {
+    DUT: uploadModerateData["DUT MS HTTP UL 30S"].Throughput,
+    REF: uploadModerateData["REF MS HTTP UL 30S"].Throughput,
+  },
+};
 
 export default Dp_MHS_httpMS_Component;

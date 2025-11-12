8717c638aeb5b945f1720a0dcc49c2097b716e1e
LiJie Su <opwinopwin@gmail.com>
2025-11-12 12:45:52 -0600

    modify play store data passing

--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Dp_playStore_Component.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import DpPlayStoreTable from './Table/DpPlayStoreTable';
+import playStoreData from '../../../DataFiles/SA/DpPlayStoreResults/Play Store.json';
 
 const Dp_playStore_Component = () => {
+  const processData = () => {
+    const processed = [];
+    const throughputCategories = ['30M', '60M', '100M'];
+    const deviceTypes = ['DUT', 'REF'];
+    const locations = ['location1', 'location2', 'location3'];
+
+    throughputCategories.forEach(throughput => {
+      deviceTypes.forEach(device => {
+        const rowData = {
+          market: 'Seattle', // Assuming market is always Seattle for now
+          throughput: throughput,
+          deviceName: device,
+          site1: null,
+          site2: null,
+          site3: null,
+          overall: null,
+        };
+
+        let siteThroughputs = [];
+
+        locations.forEach((location, index) => {
+          const siteKey = `site${index + 1}`;
+          if (playStoreData[location] && playStoreData[location][device] && playStoreData[location][device][throughput]) {
+            const dataKey = Object.keys(playStoreData[location][device][throughput])[0];
+            const value = playStoreData[location][device][throughput][dataKey].overall_average_throughput;
+            rowData[siteKey] = value !== undefined ? value.toFixed(2) : 'N/A';
+            if (value !== undefined) {
+              siteThroughputs.push(value);
+            }
+          } else {
+            rowData[siteKey] = 'N/A';
+          }
+        });
+
+        if (siteThroughputs.length > 0) {
+          const sum = siteThroughputs.reduce((acc, val) => acc + val, 0);
+          rowData.overall = (sum / siteThroughputs.length).toFixed(2);
+        } else {
+          rowData.overall = 'N/A';
+        }
+        processed.push(rowData);
+      });
+    });
+    return processed;
+  };
+
+  const tableData = processData();
+
   return (
       <div className='page-content'>
         <h2>Play-store app download test - 5G NR</h2>
-        <DpPlayStoreTable />
+        <DpPlayStoreTable tableData={tableData} />
       </div>
   );
 };
--- report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPlayStoreTable.js
+++ report-generator-app/src/ReportDetails/DataPerformance/Statoinary/Table/DpPlayStoreTable.js
@@ -1,56 +1,6 @@
 import React from 'react';
 import '../../../../../src/StyleScript/Restricted_Report_Style.css';
-import playStoreData from '../../../../DataFiles/SA/DpPlayStoreResults/Play Store.json';
-
-const DpPlayStoreTable = () => {
-  const processData = () => {
-    const processed = [];
-    const throughputCategories = ['30M', '60M', '100M'];
-    const deviceTypes = ['DUT', 'REF'];
-    const locations = ['location1', 'location2', 'location3'];
-
-    throughputCategories.forEach(throughput => {
-      deviceTypes.forEach(device => {
-        const rowData = {
-          market: 'Seattle', // Assuming market is always Seattle for now
-          throughput: throughput,
-          deviceName: device,
-          site1: null,
-          site2: null,
-          site3: null,
-          overall: null,
-        };
-
-        let siteThroughputs = [];
-
-        locations.forEach((location, index) => {
-          const siteKey = `site${index + 1}`;
-          if (playStoreData[location] && playStoreData[location][device] && playStoreData[location][device][throughput]) {
-            const dataKey = Object.keys(playStoreData[location][device][throughput])[0];
-            const value = playStoreData[location][device][throughput][dataKey].overall_average_throughput;
-            rowData[siteKey] = value !== undefined ? value.toFixed(2) : 'N/A';
-            if (value !== undefined) {
-              siteThroughputs.push(value);
-            }
-          } else {
-            rowData[siteKey] = 'N/A';
-          }
-        });
-
-        if (siteThroughputs.length > 0) {
-          const sum = siteThroughputs.reduce((acc, val) => acc + val, 0);
-          rowData.overall = (sum / siteThroughputs.length).toFixed(2);
-        } else {
-          rowData.overall = 'N/A';
-        }
-        processed.push(rowData);
-      });
-    });
-    return processed;
-  };
-
-  const tableData = processData();
-
+const DpPlayStoreTable = ({ tableData }) => {
   return (
     <div className="">
       <table className="general-table-style">
@@ -59,12 +9,13 @@ const DpPlayStoreTable = () => {
             <th rowSpan="2">Market</th>
             <th rowSpan="2">Throughput (kbps)</th>
             <th rowSpan="2">Device Name</th>
+            <th colSpan="4">Play Store Download Throughput (Mbps)</th>
           </tr>
           <tr>
             <th>Overall</th>
-            <th>Good</th>
-            <th>Moderate</th>
-            <th>Poor</th>
+            <th>Site 1</th>
+            <th>Site 2</th>
+            <th>Site 3</th>
           </tr>
         </thead>
         <tbody>

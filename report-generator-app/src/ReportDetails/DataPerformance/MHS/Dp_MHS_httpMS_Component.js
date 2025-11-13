5f94319a83e692ee25eaaf42afdcf7ad2cf2045c
LiJie Su <opwinopwin@gmail.com>
2025-11-13 15:11:57 -0600

    mhs overall table

--- report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpMS_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpMS_Component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DpMHSHttpMSTable from "./Table/DpMHSHttpMSTable";
 import DpHistogramComponent from "../DpHistogramComponent";
+import DpThroughputOverallTable from "../DpThroughputOverallTable";
 import MultiStreamHTTPData from "../../../DataFiles/SA/DpMHSResults/Multi Stream HTTP.json";
 import { CHART_COLOR_DUT, CHART_COLOR_REF } from '../../../Constants/ChartColors';
 
@@ -40,9 +41,41 @@ function Dp_MHS_httpMS_Component() {
   const overallUploadDUTMean = (dataUL.Good.DUT["Mean"] + dataUL.Moderate.DUT["Mean"]) / 2;
   const overallUploadREFMean = (dataUL.Good.REF["Mean"] + dataUL.Moderate.REF["Mean"]) / 2;
 
+  const overallTableHeader = ["Throughput", "Device Name", "Download", "Upload"];
+  const combinedOverallTableData = [
+    ["Average", "DUT", overallDownloadDUTMean.toFixed(2), overallUploadDUTMean.toFixed(2)],
+    ["Average", "REF", overallDownloadREFMean.toFixed(2), overallUploadREFMean.toFixed(2)],
+    ["Standard Deviation", "DUT", ((goodDownloadData["DUT MS HTTP DL for 30 seconds"].Throughput["Standard Deviation"] + moderateDownloadData["DUT MS HTTP DL 30s"].Throughput["Standard Deviation"]) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH01_TMO-DUT_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput["Standard Deviation"] + uploadModerateData["DUT MS HTTP UL 30S"].Throughput["Standard Deviation"]) / 2).toFixed(2)],
+    ["Standard Deviation", "REF", ((goodDownloadData["REF MS HTTP DL for 30 seconds"].Throughput["Standard Deviation"] + moderateDownloadData["REF MS HTTP DL 30s"].Throughput["Standard Deviation"]) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH02_TMO-Ref_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput["Standard Deviation"] + uploadModerateData["REF MS HTTP UL 30S"].Throughput["Standard Deviation"]) / 2).toFixed(2)],
+    ["Maximum", "DUT", ((goodDownloadData["DUT MS HTTP DL for 30 seconds"].Throughput.Maximum + moderateDownloadData["DUT MS HTTP DL 30s"].Throughput.Maximum) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH01_TMO-DUT_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput.Maximum + uploadModerateData["DUT MS HTTP UL 30S"].Throughput.Maximum) / 2).toFixed(2)],
+    ["Maximum", "REF", ((goodDownloadData["REF MS HTTP DL for 30 seconds"].Throughput.Maximum + moderateDownloadData["REF MS HTTP DL 30s"].Throughput.Maximum) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH02_TMO-Ref_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput.Maximum + uploadModerateData["REF MS HTTP UL 30S"].Throughput.Maximum) / 2).toFixed(2)],
+    ["Minimum", "DUT", ((goodDownloadData["DUT MS HTTP DL for 30 seconds"].Throughput.Minimum + moderateDownloadData["DUT MS HTTP DL 30s"].Throughput.Minimum) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH01_TMO-DUT_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput.Minimum + uploadModerateData["DUT MS HTTP UL 30S"].Throughput.Minimum) / 2).toFixed(2)],
+    ["Minimum", "REF", ((goodDownloadData["REF MS HTTP DL for 30 seconds"].Throughput.Minimum + moderateDownloadData["REF MS HTTP DL 30s"].Throughput.Minimum) / 2).toFixed(2), ((uploadGoodData["_20250915_120237_CH02_TMO-Ref_5G MHS_Multi Stream HTTP Upload for 30 seconds_Good Coverage_DA Test"].Throughput.Minimum + uploadModerateData["REF MS HTTP UL 30S"].Throughput.Minimum) / 2).toFixed(2)],
+  ];
+
   return (
     <div className='page-content'>
       <h2>MHS-httpMS Component</h2>
+      <h3>Overall Multi Stream HTTP</h3>
+      <DpThroughputOverallTable
+        tableHeader={overallTableHeader}
+        tableData={combinedOverallTableData}
+        kpiRule="Throughput"
+        kpiTargetCells={[
+          {
+            rowIndex: 0,
+            colIndex: 2,
+            dutValue: overallDownloadDUTMean.toFixed(2),
+            refValue: overallDownloadREFMean.toFixed(2),
+          },
+          {
+            rowIndex: 0,
+            colIndex: 3,
+            dutValue: overallUploadDUTMean.toFixed(2),
+            refValue: overallUploadREFMean.toFixed(2),
+          },
+        ]}
+      />
       <DpMHSHttpMSTable data={data} tableName="MHS Multi Stream HTTP Download Throughput" />
       <DpHistogramComponent
         data={[
--- report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpSS_Component.js
+++ report-generator-app/src/ReportDetails/DataPerformance/MHS/Dp_MHS_httpSS_Component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DpMHSHttpSSTable from "./Table/DpMHSHttpSSTable";
 import DpHistogramComponent from "../DpHistogramComponent";
+import DpThroughputOverallTable from "../DpThroughputOverallTable";
 import SingleStreamHTTPData from "../../../DataFiles/SA/DpMHSResults/Single Stream HTTP.json";
 import { CHART_COLOR_DUT, CHART_COLOR_REF } from '../../../Constants/ChartColors';
 
@@ -39,9 +40,41 @@ function Dp_MHS_httpSS_Component() {
   const overallUploadDUTMean = (dataUL.Good.DUT["Mean"] + dataUL.Moderate.DUT["Mean"]) / 2;
   const overallUploadREFMean = (dataUL.Good.REF["Mean"] + dataUL.Moderate.REF["Mean"]) / 2;
 
+  const overallTableHeader = ["Throughput", "Device Name", "Download", "Upload"];
+  const combinedOverallTableData = [
+    ["Average", "DUT", overallDownloadDUTMean.toFixed(2), overallUploadDUTMean.toFixed(2)],
+    ["Average", "REF", overallDownloadREFMean.toFixed(2), overallUploadREFMean.toFixed(2)],
+    ["Standard Deviation", "DUT", ((goodData["DUT SS HTTP DL for 60 seconds"].Throughput["Standard Deviation"] + moderateData["DUT SS HTTP DL 60S"].Throughput["Standard Deviation"]) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH01_TMO-DUT_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput["Standard Deviation"] + uploadModerateData["_20250918_124924_CH01_TMO-dut_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput["Standard Deviation"]) / 2).toFixed(2)],
+    ["Standard Deviation", "REF", ((goodData["REF SS HTTP DL for 60 seconds"].Throughput["Standard Deviation"] + moderateData["REF SS HTTP DL 60S"].Throughput["Standard Deviation"]) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH02_TMO-Ref_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput["Standard Deviation"] + uploadModerateData["_20250918_124924_CH02_TMO-ref_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput["Standard Deviation"]) / 2).toFixed(2)],
+    ["Maximum", "DUT", ((goodData["DUT SS HTTP DL for 60 seconds"].Throughput.Maximum + moderateData["DUT SS HTTP DL 60S"].Throughput.Maximum) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH01_TMO-DUT_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput.Maximum + uploadModerateData["_20250918_124924_CH01_TMO-dut_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput.Maximum) / 2).toFixed(2)],
+    ["Maximum", "REF", ((goodData["REF SS HTTP DL for 60 seconds"].Throughput.Maximum + moderateData["REF SS HTTP DL 60S"].Throughput.Maximum) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH02_TMO-Ref_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput.Maximum + uploadModerateData["_20250918_124924_CH02_TMO-ref_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput.Maximum) / 2).toFixed(2)],
+    ["Minimum", "DUT", ((goodData["DUT SS HTTP DL for 60 seconds"].Throughput.Minimum + moderateData["DUT SS HTTP DL 60S"].Throughput.Minimum) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH01_TMO-DUT_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput.Minimum + uploadModerateData["_20250918_124924_CH01_TMO-dut_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput.Minimum) / 2).toFixed(2)],
+    ["Minimum", "REF", ((goodData["REF SS HTTP DL for 60 seconds"].Throughput.Minimum + moderateData["REF SS HTTP DL 60S"].Throughput.Minimum) / 2).toFixed(2), ((uploadGoodData["_20250915_115630_CH02_TMO-Ref_5G MHS_Single Stream HTTP Upload of a 15 MB file_Good Coverage_DA Test"].Throughput.Minimum + uploadModerateData["_20250918_124924_CH02_TMO-ref_5G Auto_Single Stream HTTP Upload of a 15 MB file_Moderate Coverage_DA Test"].Throughput.Minimum) / 2).toFixed(2)],
+  ];
+
   return (
     <div className='page-content'>
       <h2>MHS-httpSS Component</h2>
+      <h3>Overall Single Stream HTTP</h3>
+      <DpThroughputOverallTable
+        tableHeader={overallTableHeader}
+        tableData={combinedOverallTableData}
+        kpiRule="Throughput"
+        kpiTargetCells={[
+          {
+            rowIndex: 0,
+            colIndex: 2,
+            dutValue: overallDownloadDUTMean.toFixed(2),
+            refValue: overallDownloadREFMean.toFixed(2),
+          },
+          {
+            rowIndex: 0,
+            colIndex: 3,
+            dutValue: overallUploadDUTMean.toFixed(2),
+            refValue: overallUploadREFMean.toFixed(2),
+          },
+        ]}
+      />
       <DpMHSHttpSSTable data={dataDL} tableName="MHS Single Stream HTTP Download Throughput" />
       <DpHistogramComponent
         data={[

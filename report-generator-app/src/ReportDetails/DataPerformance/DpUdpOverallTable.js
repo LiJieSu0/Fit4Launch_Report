ba508e4201c0a732e36de2bee6367ad09a323240
LiJie Su <opwinopwin@gmail.com>
2025-11-13 14:03:18 -0600

    udp dl overall table modify format

--- report-generator-app/src/ReportDetails/DataPerformance/DpUdpOverallTable.js
+++ report-generator-app/src/ReportDetails/DataPerformance/DpUdpOverallTable.js
@@ -5,6 +5,47 @@ const DpUdpOverallTable = ({ data, headers }) => {
     return <div>No data or headers available for UDP Overall Table.</div>;
   }
 
+  const processedData = [];
+  let i = 0;
+  while (i < data.length) {
+    const currentMetric = data[i]['Metric'];
+    let metricGroupStart = i;
+    let metricGroupEnd = i;
+
+    // Find the end of the current Metric group
+    while (metricGroupEnd < data.length && data[metricGroupEnd]['Metric'] === currentMetric) {
+      metricGroupEnd++;
+    }
+
+    const metricRowSpan = metricGroupEnd - metricGroupStart;
+
+    let j = metricGroupStart;
+    while (j < metricGroupEnd) {
+      const currentIdealThroughput = data[j]['Ideal Throughput'];
+      let idealThroughputGroupStart = j;
+      let idealThroughputGroupEnd = j;
+
+      // Find the end of the current Ideal Throughput sub-group within the Metric group
+      while (idealThroughputGroupEnd < metricGroupEnd && data[idealThroughputGroupEnd]['Ideal Throughput'] === currentIdealThroughput) {
+        idealThroughputGroupEnd++;
+      }
+
+      const idealThroughputRowSpan = idealThroughputGroupEnd - idealThroughputGroupStart;
+
+      for (let k = idealThroughputGroupStart; k < idealThroughputGroupEnd; k++) {
+        processedData.push({
+          ...data[k],
+          metricRowSpan: metricRowSpan,
+          isFirstInMetricGroup: k === metricGroupStart,
+          idealThroughputRowSpan: idealThroughputRowSpan,
+          isFirstInIdealThroughputGroup: k === idealThroughputGroupStart,
+        });
+      }
+      j = idealThroughputGroupEnd;
+    }
+    i = metricGroupEnd;
+  }
+
   return (
     <table className="general-table-style">
       <thead>
@@ -15,11 +56,29 @@ const DpUdpOverallTable = ({ data, headers }) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {headers.map((header, colIndex) => (
-              <td key={colIndex}>{row[header]}</td>
-            ))}
+        {processedData.map((row, pRowIndex) => (
+          <tr key={pRowIndex}>
+            {headers.map((header, colIndex) => {
+              if (header === 'Metric' && !row.isFirstInMetricGroup) {
+                return null;
+              }
+              if (header === 'Ideal Throughput' && !row.isFirstInIdealThroughputGroup) {
+                return null;
+              }
+
+              let rowSpan = 1;
+              if (header === 'Metric' && row.isFirstInMetricGroup) {
+                rowSpan = row.metricRowSpan;
+              } else if (header === 'Ideal Throughput' && row.isFirstInIdealThroughputGroup) {
+                rowSpan = row.idealThroughputRowSpan;
+              }
+
+              return (
+                <td key={colIndex} rowSpan={rowSpan}>
+                  {row[header]}
+                </td>
+              );
+            })}
           </tr>
         ))}
       </tbody>

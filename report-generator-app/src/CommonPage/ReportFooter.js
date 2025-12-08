7104354bc2ddcf14eafb09871903adf5a1f10ed1
LiJie Su <opwinopwin@gmail.com>
2025-12-07 19:07:24 -0600

    modify footer

--- report-generator-app/src/CommonPage/ReportFooter.js
+++ report-generator-app/src/CommonPage/ReportFooter.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import styles from './ReportFooter.module.css';
 
-const ReportFooter = () => {
+const ReportFooter = ({ version = '1.0', issueDate = '11/24/2025', issuer = 'Claude Li' }) => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.leftContent}>
-        Proprietary & Confidential
+      <div className={styles.headerContent}>
+        <div className={styles.leftHeader}>
+          ATMC Labs: Phase Two Report, Field Routes Confidential
+        </div>
+        <div className={styles.rightHeader}>
+          ATMC Labs
+        </div>
       </div>
-      <div className={styles.rightContent}>
-        <a href="#table-of-contents" className="back-to-top-print">Back to Top</a>
+      <div className={styles.tableContainer}>
+        <table>
+          <tbody>
+            <tr>
+              <td><b>Version:</b> {version}</td>
+              <td><b>Issue Date:</b> {issueDate}</td>
+              <td><b>Issuer:</b> {issuer}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </footer>
   );

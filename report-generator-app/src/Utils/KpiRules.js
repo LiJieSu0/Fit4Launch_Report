e601f554e54db1035fc8a8bdcca53155bbb065a4
LiJie Su <opwinopwin@gmail.com>
2025-12-09 03:24:20 -0600

    update kpi rule for cp

--- report-generator-app/src/Utils/KpiRules.js
+++ report-generator-app/src/Utils/KpiRules.js
@@ -62,6 +62,39 @@ case 'WebPageLoadTime':
         return 'var(--performance-fail)';
       }
       break;
+    case 'CallSetupTime':
+      if (dutValue < refValue) {
+        return 'var(--performance-excellent)';
+      } else if (dutValue >= refValue && dutValue <= 1.1 * refValue) {
+        return 'var(--performance-pass)';
+      } else if (dutValue > 1.1 * refValue && dutValue <= 1.25 * refValue) {
+        return 'var(--performance-marginal-fail)';
+      } else if (dutValue > 1.25 * refValue) {
+        return 'var(--performance-fail)';
+      }
+      break;
+    case 'CallRetention':
+      if (dutValue > 0.99) { // If p-value for Fisher Exact > 0.99
+        return 'var(--performance-excellent)';
+      } else if (dutValue >= 0.05 && dutValue <= 0.99) { // If p-value for Fisher Exact is between 0.05 and 0.99
+        return 'var(--performance-pass)';
+      } else if (false) { // TODO Marginal Fail: If RED condition is satisfied but % MO Failures (DUT) < 1% (placeholder)
+        return 'var(--performance-marginal-fail)';
+      } else if (dutValue < 0.05) { // If p-value for Fisher Exact < 0.05
+        return 'var(--performance-fail)';
+      }
+      break;
+    case 'CallInitiation':
+      if (dutValue > 0.99) { // If p-value for Fisher Exact > 0.99
+        return 'var(--performance-excellent)';
+      } else if (dutValue >= 0.05 && dutValue <= 0.99) { // If p-value for Fisher Exact is between 0.05 and 0.99
+        return 'var(--performance-pass)';
+      } else if (false) { // TODO Marginal Fail: If RED condition is satisfied but % MO/MT Failures (DUT) < 1% (placeholder)
+        return 'var(--performance-marginal-fail)';
+      } else if (dutValue < 0.05) { // If p-value for Fisher Exact < 0.05
+        return 'var(--performance-fail)';
+      }
+      break;
     default:
       return; // Default case if kpiType doesn't match
   }

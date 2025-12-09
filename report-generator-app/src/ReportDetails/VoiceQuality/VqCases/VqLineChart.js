eed4111b714ea8a7145c3d0f57e1070e6ecd201f
LiJie Su <opwinopwin@gmail.com>
2025-12-09 17:33:48 -0600

    modify vq line chart size

--- report-generator-app/src/ReportDetails/VoiceQuality/VqCases/VqLineChart.js
+++ report-generator-app/src/ReportDetails/VoiceQuality/VqCases/VqLineChart.js
@@ -57,7 +57,7 @@ const VqLineChart = ({ dataSource }) => {
   }
 
   return (
-    <div style={{ width: '60%', height: 400 }}>
+    <div style={{ width: '50%', height: 300 }}>
       <ResponsiveContainer>
         <LineChart
           data={chartData}

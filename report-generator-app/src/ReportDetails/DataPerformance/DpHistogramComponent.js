2da6c5e2c748cd99d6648cc444ff8c1f3dd01956
LiJie Su <opwinopwin@gmail.com>
2025-12-09 19:12:56 -0600

    modify bar chart

--- report-generator-app/src/ReportDetails/DataPerformance/DpHistogramComponent.js
+++ report-generator-app/src/ReportDetails/DataPerformance/DpHistogramComponent.js
@@ -12,34 +12,36 @@ import {
 } from 'recharts';
 
 const DpHistogramComponent = ({ data, title, yAxisLabel, barKeys }) => {
+  const chartWidth = data.length === 1 ? 300 : 500;
   return (
     <div>
 
-    <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>{title}</h3>
-    {/* <div style={{ width: '100%', height: '400px', minHeight: '250px' }}> */}
-    <div>
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis label={{ value: yAxisLabel, angle: -90, position: 'insideLeft' }} />
-        <Tooltip />
-        <Legend />
-        {Array.isArray(barKeys) && barKeys.map((bar, index) => (
-          <Bar key={index} dataKey={bar.key} fill={bar.fill}>
-          </Bar>
-        ))}
-      </BarChart>
-    </div>
+      <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>{title}</h3>
+      {/* <div style={{ width: '100%', height: '400px', minHeight: '250px' }}> */}
+      <div>
+        <BarChart
+          width={chartWidth}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barGap={10}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis label={{ value: yAxisLabel, angle: -90, position: 'insideLeft' }} />
+          <Tooltip />
+          <Legend />
+          {Array.isArray(barKeys) && barKeys.map((bar, index) => (
+            <Bar key={index} dataKey={bar.key} fill={bar.fill} barSize={60}>
+            </Bar>
+          ))}
+        </BarChart>
+      </div>
     </div>
 
   );

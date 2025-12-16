895f51b13c929f1e0956ff287f3cf774ad1aa0a4
LiJie Su <opwinopwin@gmail.com>
2025-12-16 14:40:01 -0600

    add offset for the base station

--- report-generator-app/src/ReportDetails/CoveragePerformance/CoverageMap.js
+++ report-generator-app/src/ReportDetails/CoveragePerformance/CoverageMap.js
@@ -6,26 +6,36 @@ import L from 'leaflet';
 // Fix for default icon issues with Leaflet and Webpack
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
+
+
+const LONGITUDE_OFFSET = 0.04; 
 
 const CoverageMap = ({ baseStationCoords }) => {
   if (!baseStationCoords || baseStationCoords.length === 0) {
     return <div>No base station coordinates provided.</div>;
   }
 
-  // Assuming baseStationCoords is an array of [latitude, longitude]
-  const position = baseStationCoords;
+  const originalLat = baseStationCoords[0];
+  const originalLon = baseStationCoords[1];
+
+  const adjustedLon = originalLon + LONGITUDE_OFFSET;
+  const position = [originalLat, adjustedLon]; 
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '500px', width: '100%' }}>
+    <MapContainer 
+      center={position} 
+      zoom={13} 
+      style={{ height: '400px', width: '80%' }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={baseStationCoords}> 
       </Marker>
     </MapContainer>
   );

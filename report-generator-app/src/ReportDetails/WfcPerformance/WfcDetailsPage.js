97dd4f329bd2e4154f771587d8523d376a826c1d
LiJie Su <opwinopwin@gmail.com>
2026-01-26 13:11:07 -0600

    add wfc test details page

--- report-generator-app/src/ReportDetails/WfcPerformance/WfcDetailsPage.js
+++ report-generator-app/src/ReportDetails/WfcPerformance/WfcDetailsPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../../StyleScript/Restricted_Report_Style.css';
-
+import WfcTestDetailsPage from './WfcTestDetailsPage';
 const WfcDetailsPage = () => {
 
   return (
     <>
-    
+      <WfcTestDetailsPage tc="TC150" label="Call Performance Baseline" />
+      <WfcTestDetailsPage tc="TC151" label="Call Performance Baseline" />
+      <WfcTestDetailsPage tc="TC152" label="Call Performance" />
+      <WfcTestDetailsPage tc="TC153" label="Call Performance" />
+      <WfcTestDetailsPage tc="TC154" label="Call Performance" />
+      <WfcTestDetailsPage tc="TC155" label="Call Performance" />
+      <WfcTestDetailsPage tc="TC156" label="Call Performance" />
+      <WfcTestDetailsPage tc="TC157" label="Call Performance" />
     </>
   );
 };
---
+++ report-generator-app/src/ReportDetails/WfcPerformance/WfcTestDetailsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../StyleScript/Restricted_Report_Style.css';
+
+const WfcTestDetailsPage = () => {
+
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default WfcTestDetailsPage;

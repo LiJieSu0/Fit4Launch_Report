365ed1f48ab2769989d810dbe1742e085d0e8e7c
LiJie Su <opwinopwin@gmail.com>
2026-02-01 06:25:34 -0600

    update device info

--- report-generator-app/src/CommonPage/DeviceInfoPage.js
+++ report-generator-app/src/CommonPage/DeviceInfoPage.js
@@ -4,25 +4,19 @@ import DynamicHeader from './DynamicHeader';
 
 const deviceData = [
     {
-        testDeviceLabel: "Samsung XCover Pro7",
+        testDeviceLabel: "Motorola XT2575-4",
         role: "Device Under Test",
-        softwareVersion: "SP1A.210812.016.A125USQU3CVG4",
+        softwareVersion: "17.0.02.147_Development Version",
         imei: [
-            "354879770010297", "354879770011428", "354879770012160",
-            "354879770012335", "354879770011139", "354879770011535",
-            "354879770010388", "354879770012210", "354879770011048",
-            "354879770012475"
+            "352274860000855", "352274860001077", "352274860000996 ", "352274860001119 "
         ]
     },
     {
-        testDeviceLabel: "Samsung GS25 FE",
+        testDeviceLabel: "Samsung Galaxy A14 5G (SM-A146U) ",
         role: "Reference",
-        softwareVersion: "BP2A.250605.031.A3.S731USQU1AYH5",
+        softwareVersion: "AP3A.240905.015.A2.A146USQSFEYK3",
         imei: [
-            "358625370021547", "358625370020499", "358625370022602",
-            "358625370021935", "358625370022487", "358625370020408",
-            "358625370022867", "358625370021489", "358625370021489",
-            "358625370021596"
+            "350545460016108", "350545460016157", "350545460014939", "350545460015225"
         ]
     }
 ];

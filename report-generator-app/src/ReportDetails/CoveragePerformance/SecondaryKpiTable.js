eab89a086893f5f61023e267b67e491ee65fbd91
LiJie Su <opwinopwin@gmail.com>
2026-02-25 13:13:32 -0600

    add secondary kpi color

--- report-generator-app/src/ReportDetails/CoveragePerformance/SecondaryKpiTable.js
+++ report-generator-app/src/ReportDetails/CoveragePerformance/SecondaryKpiTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getKpiCellColor } from '../../Utils/KpiRules';
 
 const SecondaryKpiTable = ({ data }) => (
     <table className="general-table-style">
@@ -27,9 +28,18 @@ const SecondaryKpiTable = ({ data }) => (
                 <React.Fragment key={runIndex}>
                     {runData.segments.map((segmentData, segmentIndex) => {
                         const totalSegments = runData.segments.length;
-                        const isFirst30 = segmentIndex < Math.ceil(totalSegments * 0.3);
-                        const isLast30 = segmentIndex >= totalSegments - Math.ceil(totalSegments * 0.3);
-                        const highlightClass = (isFirst30 || isLast30) ? 'highlight-beige' : '';
+
+                        const dutBler = segmentData.DUT?.bler;
+                        const refBler = segmentData.REF?.bler;
+                        const blerColor = getKpiCellColor('SecondaryBler', dutBler, refBler);
+
+                        const dutDlMcs = segmentData.DUT?.dlMcs;
+                        const refDlMcs = segmentData.REF?.dlMcs;
+                        const dlMcsColor = getKpiCellColor('SecondaryMcs', dutDlMcs, refDlMcs);
+
+                        const dutUlMcs = segmentData.DUT?.ulMcs;
+                        const refUlMcs = segmentData.REF?.ulMcs;
+                        const ulMcsColor = getKpiCellColor('SecondaryMcs', dutUlMcs, refUlMcs);
 
                         return (
                             <tr
@@ -39,23 +49,37 @@ const SecondaryKpiTable = ({ data }) => (
                                 {segmentIndex === 0 && (
                                     <td className="run-divider" rowSpan={totalSegments}>{runData.run}</td>
                                 )}
-                                <td className={highlightClass}>{segmentData.segment}</td>
-                                <td className={highlightClass}>{segmentData.DUT && typeof segmentData.DUT.bler === 'number' ? segmentData.DUT.bler.toFixed(2) : '0.00'}</td>
-                                <td className={highlightClass}>{segmentData.REF && typeof segmentData.REF.bler === 'number' ? segmentData.REF.bler.toFixed(2) : '0.00'}</td>
-                                <td className={highlightClass}>{segmentData.DUT && typeof segmentData.DUT.dlMcs === 'number' ? segmentData.DUT.dlMcs.toFixed(2) : '0.00'}</td>
-                                <td className={highlightClass}>{segmentData.REF && typeof segmentData.REF.dlMcs === 'number' ? segmentData.REF.dlMcs.toFixed(2) : '0.00'}</td>
-                                <td className={highlightClass}>{segmentData.DUT && typeof segmentData.DUT.ulMcs === 'number' ? segmentData.DUT.ulMcs.toFixed(2) : '0.00'}</td>
-                                <td className={highlightClass}>{segmentData.REF && typeof segmentData.REF.ulMcs === 'number' ? segmentData.REF.ulMcs.toFixed(2) : '0.00'}</td>
+                                <td>{segmentData.segment}</td>
+                                <td style={{ backgroundColor: blerColor || '', color: blerColor ? 'black' : '' }}>
+                                    {typeof dutBler === 'number' ? dutBler.toFixed(2) : '0.00'}
+                                </td>
+                                <td>{typeof refBler === 'number' ? refBler.toFixed(2) : '0.00'}</td>
+                                <td style={{ backgroundColor: dlMcsColor || '', color: dlMcsColor ? 'black' : '' }}>
+                                    {typeof dutDlMcs === 'number' ? dutDlMcs.toFixed(2) : '0.00'}
+                                </td>
+                                <td>{typeof refDlMcs === 'number' ? refDlMcs.toFixed(2) : '0.00'}</td>
+                                <td style={{ backgroundColor: ulMcsColor || '', color: ulMcsColor ? 'black' : '' }}>
+                                    {typeof dutUlMcs === 'number' ? dutUlMcs.toFixed(2) : '0.00'}
+                                </td>
+                                <td>{typeof refUlMcs === 'number' ? refUlMcs.toFixed(2) : '0.00'}</td>
 
-                                {/* Render TxPower only on the first row of the run, spanning all segments */}
                                 {segmentIndex === 0 && (
                                     <>
-                                        <td className="run-divider" rowSpan={totalSegments}>
-                                            {runData.txPower && typeof runData.txPower.DUT === 'number' ? runData.txPower.DUT.toFixed(2) : '0.00'}
-                                        </td>
-                                        <td className="run-divider" rowSpan={totalSegments}>
-                                            {runData.txPower && typeof runData.txPower.REF === 'number' ? runData.txPower.REF.toFixed(2) : '0.00'}
-                                        </td>
+                                        {(() => {
+                                            const dutTx = runData.txPower?.DUT;
+                                            const refTx = runData.txPower?.REF;
+                                            const txColor = getKpiCellColor('SecondaryTxPower', dutTx, refTx);
+                                            return (
+                                                <>
+                                                    <td className="run-divider" rowSpan={totalSegments} style={{ backgroundColor: txColor || '', color: txColor ? 'black' : '' }}>
+                                                        {typeof dutTx === 'number' ? dutTx.toFixed(2) : '0.00'}
+                                                    </td>
+                                                    <td className="run-divider" rowSpan={totalSegments}>
+                                                        {typeof refTx === 'number' ? refTx.toFixed(2) : '0.00'}
+                                                    </td>
+                                                </>
+                                            );
+                                        })()}
                                     </>
                                 )}
                             </tr>
